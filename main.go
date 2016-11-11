6ca33f27b52409e8d8c65230a62f9d1912379953
Chris Shiels <chris@systems-engineers.co.uk>
2016-11-11 19:49:17 +0000

    Added command line parsing.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 
 
 import (
+    "flag"
     "fmt"
     "os"
 )
@@ -15,31 +16,110 @@ const exitsuccess = 0
 const exitfailure = 1
 
 
+func mainshow(stdin *os.File,
+              stdout *os.File,
+              stderr *os.File,
+              args []string,
+              verbose bool) (exitstatus int) {
+    flagset := flag.NewFlagSet(args[0], flag.ExitOnError)
+
+    flagset.Usage = func() {
+        fmt.Fprintln(stdout, "Usage:  mp3adora [ -v ] show [ filename ...]")
+        flag.PrintDefaults()
+    }
+
+    // Note flagset.Parse() will also handle '-h' and '--help' and will exit
+    // with exit status 2.
+    flagset.Parse(args[1:])
+
+    mp3adorashowhandler := newmp3adorashowhandler(stdout, stderr)
+    mp3adora := newmp3adora(mp3adorashowhandler)
+    var file *os.File
+    var size int
+    var err error
+
+    if len(flagset.Args()) == 0 {
+        if size, err = mp3adora.parse(os.Stdin); err != nil {
+            fmt.Fprintf(stderr, "mp3adora: %s\n", err)
+            return exitfailure
+        }
+
+        fmt.Fprintf(stdout, "size:  %d\n", size)
+    } else if len(flagset.Args()) == 1 {
+        if file, err = os.Open(flagset.Args()[0]); err != nil {
+            fmt.Fprintf(stderr, "mp3adora: %s\n", err)
+            return exitfailure
+        }
+
+        defer file.Close()
+
+        if size, err = mp3adora.parse(file); err != nil {
+            fmt.Fprintf(stderr, "mp3adora: %s\n", err)
+            return exitfailure
+        }
+
+        fmt.Fprintf(stdout, "size:  %d\n", size)
+    } else {
+        for i, filename := range flagset.Args() {
+            if i > 0 {
+                fmt.Fprintln(stdout)
+            }
+            fmt.Fprintf(stdout, "%d %s:\n", i, filename)
+
+            if file, err = os.Open(flagset.Args()[0]); err != nil {
+                fmt.Fprintf(stderr, "mp3adora: %s\n", err)
+                return exitfailure
+            }
+
+            defer file.Close()
+
+            if size, err = mp3adora.parse(file); err != nil {
+                fmt.Fprintf(stderr, "mp3adora: %s\n", err)
+                return exitfailure
+            }
+
+            fmt.Fprintf(stdout, "size:  %d\n", size)
+        }
+    }
+
+    return exitsuccess
+}
+
+
 func _main(stdin *os.File,
            stdout *os.File,
            stderr *os.File,
            args []string) (exitstatus int) {
-    mp3adorashowhandler := newmp3adorashowhandler(stdout, stderr)
-    mp3adora := newmp3adora(mp3adorashowhandler)
+    flagset := flag.NewFlagSet(args[0], flag.ExitOnError)
 
-    var file *os.File
-    var err error
-    if file, err = os.Open("file.mp3"); err != nil {
-        fmt.Fprintf(stderr, "mp3adora: %s\n", err)
+    flagset.Usage = func() {
+        fmt.Fprintln(stdout, "Usage:  mp3adora [ -v ] command [ options ... ]")
+        fmt.Fprintln(stdout)
+        fmt.Fprintln(stdout, "Valid commands are:")
+        fmt.Fprintln(stdout, "show")
+        flag.PrintDefaults()
+    }
+
+    flagv := flagset.Bool("v",
+                          false,
+                          "Verbose")
+
+    // Note flagset.Parse() will also handle '-h' and '--help' and will exit
+    // with exit status 2.
+    flagset.Parse(args[1:])
+
+    if len(flagset.Args()) == 0 {
+        flagset.Usage()
         return exitfailure
     }
 
-    defer file.Close()
-
-    var size int
-    if size, err = mp3adora.parse(file); err != nil {
-        fmt.Fprintf(stderr, "mp3adora: %s\n", err)
-        return exitfailure
+    switch {
+        case flagset.Args()[0] == "show":
+            return mainshow(stdin, stdout, stderr, flagset.Args(), *flagv)
     }
 
-    fmt.Printf("size:  %d\n", size)
-
-    return exitsuccess
+    flagset.Usage()
+    return exitfailure
 }
 
 
