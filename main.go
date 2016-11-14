3bc770534e4c3acdebf329b66b0c03b0d1e608a1
Chris Shiels <chris@systems-engineers.co.uk>
2016-11-14 19:59:36 +0000

    Refactored argument passing from main() to mainshow() and refactored mainshow() to mainshow() and show().

--- main.go
+++ main.go
@@ -16,12 +16,39 @@ const exitsuccess = 0
 const exitfailure = 1
 
 
+func show(stdin *os.File,
+          stdout *os.File,
+          stderr *os.File,
+          filename string,
+          verbose bool) (size int, err error) {
+    mp3adorashowhandler := newmp3adorashowhandler(stdout, stderr)
+    mp3adora := newmp3adora(mp3adorashowhandler)
+
+    var file *os.File
+    if filename != "" {
+        if file, err = os.Open(filename); err != nil {
+            return 0, err
+        }
+    } else {
+        file = stdin
+    }
+
+    defer file.Close()
+
+    if size, err = mp3adora.parse(file); err != nil {
+        return 0, err
+    }
+
+    return size, nil
+}
+
+
 func mainshow(stdin *os.File,
               stdout *os.File,
               stderr *os.File,
               args []string,
               verbose bool) (exitstatus int) {
-    flagset := flag.NewFlagSet(args[0], flag.ExitOnError)
+    flagset := flag.NewFlagSet("show", flag.ExitOnError)
 
     flagset.Usage = func() {
         fmt.Fprintln(stdout, "Usage:  mp3adora [ -v ] show [ filename ... ]")
@@ -30,54 +57,27 @@ func mainshow(stdin *os.File,
 
     // Note flagset.Parse() will also handle '-h' and '--help' and will exit
     // with exit status 2.
-    flagset.Parse(args[1:])
-
-    mp3adorashowhandler := newmp3adorashowhandler(stdout, stderr)
-    mp3adora := newmp3adora(mp3adorashowhandler)
-    var file *os.File
-    var size int
-    var err error
+    flagset.Parse(args)
 
     if len(flagset.Args()) == 0 {
-        if size, err = mp3adora.parse(os.Stdin); err != nil {
+        size, err := show(stdin, stdout, stderr, "", verbose)
+        if err != nil {
             fmt.Fprintf(stderr, "mp3adora: %s\n", err)
             return exitfailure
         }
-
-        fmt.Fprintf(stdout, "size:  %d\n", size)
-    } else if len(flagset.Args()) == 1 {
-        if file, err = os.Open(flagset.Args()[0]); err != nil {
-            fmt.Fprintf(stderr, "mp3adora: %s\n", err)
-            return exitfailure
-        }
-
-        defer file.Close()
-
-        if size, err = mp3adora.parse(file); err != nil {
-            fmt.Fprintf(stderr, "mp3adora: %s\n", err)
-            return exitfailure
-        }
-
         fmt.Fprintf(stdout, "size:  %d\n", size)
     } else {
         for i, filename := range flagset.Args() {
             if i > 0 {
                 fmt.Fprintln(stdout)
             }
-            fmt.Fprintf(stdout, "%d %s:\n", i, filename)
+            fmt.Fprintf(stdout, "%s:\n", filename)
 
-            if file, err = os.Open(flagset.Args()[0]); err != nil {
+            size, err := show(stdin, stdout, stderr, filename, verbose)
+            if err != nil {
                 fmt.Fprintf(stderr, "mp3adora: %s\n", err)
                 return exitfailure
             }
-
-            defer file.Close()
-
-            if size, err = mp3adora.parse(file); err != nil {
-                fmt.Fprintf(stderr, "mp3adora: %s\n", err)
-                return exitfailure
-            }
-
             fmt.Fprintf(stdout, "size:  %d\n", size)
         }
     }
@@ -115,7 +115,7 @@ func _main(stdin *os.File,
 
     switch {
         case flagset.Args()[0] == "show":
-            return mainshow(stdin, stdout, stderr, flagset.Args(), *flagv)
+            return mainshow(stdin, stdout, stderr, flagset.Args()[1:], *flagv)
     }
 
     flagset.Usage()
